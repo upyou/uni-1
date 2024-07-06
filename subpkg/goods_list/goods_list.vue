669397c5d24f0755db83b4e67a42c27d11f4d48d
user <3573643522@qq.com>
2024-07-06 22:07:49 +0800

    goods_list

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,18 @@
 <template>
-	<view>
-		goods_list
+	<view class="goods-list">
+		<view class="goods-item" v-for="(item,i) in goodsList" :key="i" @click="gotoGoodsDetail(item)">
+			<view class="goods-item-left">
+				<image :src="item.goods_small_logo || defaultPic" mode=""></image>
+			</view>
+			<view class="goods-item-right">
+				<view class="goods-item-goods_name" style="font-size: 13px;">
+					{{item.goods_name}}
+				</view>
+				<view class="goods-item-price">
+					￥{{item.goods_price.toFixed(2)}}
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +20,82 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj:{
+					query:'',
+					cid:'',
+					pagenum:1,
+					pagesize:10
+				},
+				goodsList:[],
+				total:0,
+				defaultPic:'',
+				isLoading:false
 			}
 		},
-		methods: {
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			this.getGoodsList()
 			
+		},
+		onPullDownRefresh() {
+			this.goodsList = []
+			this.queryObj.pagenum = 1
+			this.getGoodsList()
+			setTimeout(()=>{
+				uni.stopPullDownRefresh()
+			},3000)
+		},
+		onReachBottom() {
+			if(this.isLoading) return
+			this.queryObj.pagenum = this.queryObj.pagenum + 1
+			if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total)
+			return uni.showToast({
+					title:"加载完毕",
+					duration:1500
+				})
+			 this.getGoodsList()
+			 
+		},
+		methods: {
+			async getGoodsList(){
+				this.isLoading = true
+				const {data:res} = await uni.$http.get('/goods/search',this.queryObj)
+				this.total = res.message.total
+				this.goodsList = [...this.goodsList,...res.message.goods]
+				this.isLoading = false
+				console.log(res);
+			},
+			gotoGoodsDetail(item){
+				uni.navigateTo({
+					url:'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+				})
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+.goods-list{
+	.goods-item{
+		display: flex;
+		border-bottom: 1px solid #7f7f7f;
+		.goods-item-left{
+			image{
+				width: 120px;
+				height: 120px;
+				margin-right: 20px;
+				padding: 10px;
+			}
+		}
+		.goods-item-right{
+			display: flex;
+			flex-direction: column;
+			justify-content:space-around;
+			.goods-item-price{
+				color: red;
+			}
+		}
+	}
+}
 </style>
--- unpackage/dist/dev/mp-weixin/subpkg/goods_list/goods_list.js
+++ unpackage/dist/dev/mp-weixin/subpkg/goods_list/goods_list.js
@@ -2,12 +2,71 @@
 const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
-    return {};
+    return {
+      queryObj: {
+        query: "",
+        cid: "",
+        pagenum: 1,
+        pagesize: 10
+      },
+      goodsList: [],
+      total: 0,
+      defaultPic: "",
+      isLoading: false
+    };
   },
-  methods: {}
+  onLoad(options) {
+    this.queryObj.query = options.query || "";
+    this.queryObj.cid = options.cid || "";
+    this.getGoodsList();
+  },
+  onPullDownRefresh() {
+    this.goodsList = [];
+    this.queryObj.pagenum = 1;
+    this.getGoodsList();
+    setTimeout(() => {
+      common_vendor.index.stopPullDownRefresh();
+    }, 3e3);
+  },
+  onReachBottom() {
+    if (this.isLoading)
+      return;
+    this.queryObj.pagenum = this.queryObj.pagenum + 1;
+    if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total)
+      return common_vendor.index.showToast({
+        title: "加载完毕",
+        duration: 1500
+      });
+    this.getGoodsList();
+  },
+  methods: {
+    async getGoodsList() {
+      this.isLoading = true;
+      const { data: res } = await common_vendor.index.$http.get("/goods/search", this.queryObj);
+      this.total = res.message.total;
+      this.goodsList = [...this.goodsList, ...res.message.goods];
+      this.isLoading = false;
+      console.log(res);
+    },
+    gotoGoodsDetail(item) {
+      common_vendor.index.navigateTo({
+        url: "/subpkg/goods_detail/goods_detail?goods_id=" + item.goods_id
+      });
+    }
+  }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return {
+    a: common_vendor.f($data.goodsList, (item, i, i0) => {
+      return {
+        a: item.goods_small_logo || $data.defaultPic,
+        b: common_vendor.t(item.goods_name),
+        c: common_vendor.t(item.goods_price.toFixed(2)),
+        d: i,
+        e: common_vendor.o(($event) => $options.gotoGoodsDetail(item), i)
+      };
+    })
+  };
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/HBuilderX/uni-1/subpkg/goods_list/goods_list.vue"]]);
 wx.createPage(MiniProgramPage);
