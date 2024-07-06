1f251f488e713a2f41d92437dd900fc95c59775a
user <3573643522@qq.com>
2024-07-06 23:20:39 +0800

    goodsdetail完成

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,34 @@
 <template>
-	<view>
-		goods_detail
+	<view v-if="goodsInfo.goods_name" style="padding-bottom: 50px;">
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular="true">
+			<swiper-item v-for="(item,i) in goodsInfo.pics">
+				<image :src="item.pics_big" @click="preview(i)"></image>
+			</swiper-item>
+		</swiper>
+		<view class="goods-info-box">
+			<view class="price">
+				￥{{goodsInfo.goods_price}}
+			</view>
+			<view class="goods-info-body">
+				<view class="goods-info-name">
+					{{goodsInfo.goods_name}}
+				</view>
+				<view class="goods-info-icon">
+					<uni-icons type="star" size="18" color="gray"></uni-icons>
+					<text>收藏</text>
+				</view>
+			</view>
+			<view class="yun">
+				快递：免运费
+			</view>
+		</view>
+		<rich-text :nodes="goodsInfo.goods_introduce"></rich-text>
+		<view class="goods-carts">
+					<uni-goods-nav 
+					:options="options" :fill="true" 
+					:button-group="buttonGroup" @click="onClick"
+					@buttonClick="buttonClick" />
+		</view>
 	</view>
 </template>
 
@@ -8,15 +36,104 @@
 	export default {
 		data() {
 			return {
-				
+				id:0,
+				goodsInfo:{},
+				options: [
+					{
+						icon:'shop',
+						text:'店铺'
+					},
+					{
+						icon:'cart',
+						text:'购物车',
+						info:0
+					}
+				],
+				buttonGroup:[
+					{
+						text: '加入购物车',
+						backgroundColor: 'linear-gradient(90deg, #FFCD1E, #FF8A18)',
+						color: '#fff'
+					},
+					{
+						text: '立即购买',
+						backgroundColor: 'linear-gradient(90deg, #FE6035, #EF1224)',
+						color: '#fff'
+					}
+				],
 			}
+				
+		},
+		onLoad(option) {
+			this.id = option.goods_id
+			this.getGoodsDetail()
 		},
 		methods: {
-			
+			async getGoodsDetail(){
+				const {data:res} = await uni.$http.get('/goods/detail?goods_id='+ this.id)
+				res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g,'<img style="display:block;"').replace(/webp/g, 'jpg')
+				this.goodsInfo = res.message
+			},
+			preview(i){
+				uni.previewImage({
+					current:i,
+					urls:this.goodsInfo.pics.map(i => i.pics_big)
+				})
+			},
+			onClick(e){
+				if(e.content.text === '购物车'){
+					uni.switchTab({
+						url:'/pages/Cart/Cart'
+					})
+				}
+			}
+		
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+swiper{
+	height: 750rpx;
+	image{
+		width: 100%;
+		height: 100%;
+	}
+}
+.goods-info-box{
+	padding: 6px;
+	.price{
+		font-size: 18px;
+		color: red;
+		margin: 8px;
+	}
+	.goods-info-body{
+		display: flex;
+		justify-content: space-between;
+		.goods-info-name{
+			font-size: 12px;
+			margin-right: 10px;
+		}
+		.goods-info-icon{
+			width: 120px;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			border-left: 1px solid #e7e7e7;
+			color: gray;
+		}
+	}
+	.yun{
+		font-size: 12px;
+		margin: 10px 0;
+		color: gray;
+	}
+}
+.goods-carts{
+	position: fixed;
+	bottom: 0;
+	left: 0;
+	width: 100%;
+}
 </style>
--- unpackage/dist/dev/mp-weixin/subpkg/goods_detail/goods_detail.js
+++ unpackage/dist/dev/mp-weixin/subpkg/goods_detail/goods_detail.js
@@ -2,12 +2,95 @@
 const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
-    return {};
+    return {
+      id: 0,
+      goodsInfo: {},
+      options: [
+        {
+          icon: "shop",
+          text: "店铺"
+        },
+        {
+          icon: "cart",
+          text: "购物车",
+          info: 0
+        }
+      ],
+      buttonGroup: [
+        {
+          text: "加入购物车",
+          backgroundColor: "linear-gradient(90deg, #FFCD1E, #FF8A18)",
+          color: "#fff"
+        },
+        {
+          text: "立即购买",
+          backgroundColor: "linear-gradient(90deg, #FE6035, #EF1224)",
+          color: "#fff"
+        }
+      ]
+    };
   },
-  methods: {}
+  onLoad(option) {
+    this.id = option.goods_id;
+    this.getGoodsDetail();
+  },
+  methods: {
+    async getGoodsDetail() {
+      const { data: res } = await common_vendor.index.$http.get("/goods/detail?goods_id=" + this.id);
+      res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;"').replace(/webp/g, "jpg");
+      this.goodsInfo = res.message;
+    },
+    preview(i) {
+      common_vendor.index.previewImage({
+        current: i,
+        urls: this.goodsInfo.pics.map((i2) => i2.pics_big)
+      });
+    },
+    onClick(e) {
+      if (e.content.text === "购物车") {
+        common_vendor.index.switchTab({
+          url: "/pages/Cart/Cart"
+        });
+      }
+    }
+  }
 };
+if (!Array) {
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  const _easycom_uni_goods_nav2 = common_vendor.resolveComponent("uni-goods-nav");
+  (_easycom_uni_icons2 + _easycom_uni_goods_nav2)();
+}
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+const _easycom_uni_goods_nav = () => "../../uni_modules/uni-goods-nav/components/uni-goods-nav/uni-goods-nav.js";
+if (!Math) {
+  (_easycom_uni_icons + _easycom_uni_goods_nav)();
+}
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return common_vendor.e({
+    a: $data.goodsInfo.goods_name
+  }, $data.goodsInfo.goods_name ? {
+    b: common_vendor.f($data.goodsInfo.pics, (item, i, i0) => {
+      return {
+        a: item.pics_big,
+        b: common_vendor.o(($event) => $options.preview(i))
+      };
+    }),
+    c: common_vendor.t($data.goodsInfo.goods_price),
+    d: common_vendor.t($data.goodsInfo.goods_name),
+    e: common_vendor.p({
+      type: "star",
+      size: "18",
+      color: "gray"
+    }),
+    f: $data.goodsInfo.goods_introduce,
+    g: common_vendor.o($options.onClick),
+    h: common_vendor.o(_ctx.buttonClick),
+    i: common_vendor.p({
+      options: $data.options,
+      fill: true,
+      ["button-group"]: $data.buttonGroup
+    })
+  } : {});
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/HBuilderX/uni-1/subpkg/goods_detail/goods_detail.vue"]]);
 wx.createPage(MiniProgramPage);
